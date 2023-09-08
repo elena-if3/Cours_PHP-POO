3b0ee0376747690903f882de68b03256c13db9a6
Elena Rottin <elena.rottin@gmail.com>
2023-09-08 12:21:55 +0200

    minor change

--- 10.Composer/ImageManipulation/accueil.php
+++ 10.Composer/ImageManipulation/accueil.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Manipulation d'images - Spatie</title>
 </head>
 <body>
     <?php
@@ -11,23 +11,20 @@
 
     use Spatie\Image\Image;
 
-    // Image::load("./images/Nokota_Horses_cropped.jpg")
-    //     ->greyscale ()
-    //     ->save ("./images/horses-grey.jpg");
+    Image::load("./images/Nokota_Horses_cropped.jpg")
+        ->greyscale ()
+        ->save ("./images/horses-grey.jpg");
 
-    // $im3 = new Image ("./images/horses.jpg");
-    // $im3->sepia ();
-    // $im3->save ("./images/horses-sepia.jpg");
+    $im3 = new Image ("./images/horses.jpg");
+    $im3->sepia ();
+    $im3->save ("./images/horses-sepia.jpg");
 
-    // utiliser Faker
-    use Faker\Factory;
+    // // utiliser Faker
+    // use Faker\Factory;
 
-    $faker = Factory::create("bn_BD");
-    $unNom = $faker->name();
-    echo ("<h4>Bonjour " . $unNom . "</h4>");
-
-    
-
+    // $faker = Factory::create("bn_BD");
+    // $unNom = $faker->name();
+    // echo ("<h4>Bonjour " . $unNom . "</h4>");
 
     
     ?>
